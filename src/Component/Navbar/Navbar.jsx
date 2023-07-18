e222964e1658c0be4a2c84167786a83417add658
azharrahad31 <azherudinrahad@gmail.com>
2023-07-18 12:13:11 +0600

    first commit

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='navbar-section'>
+            
+            
+            
+        </div>
+    );
+};
+
+export default Navbar;
